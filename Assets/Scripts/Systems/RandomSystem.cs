746a9a24104e7e35148116f07567bda48dc63a4a
AD\u1330244 <debdipbanerjee7@gmail.com>
2022-11-21 19:43:54 -0700

    Random number generation for multithreaded system

---
+++ Assets/Scripts/Systems/RandomSystem.cs
@@ -0,0 +1,36 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel.Unsafe;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public class RandomSystem : SystemBase
+{
+    public NativeArray<Unity.Mathematics.Random> RandomArray { get; private set; }
+
+    protected override void OnCreate()
+    {
+        var randomArray = new Unity.Mathematics.Random[JobsUtility.MaxJobThreadCount];
+        var seed = new System.Random();
+
+        for (int i = 0; i < JobsUtility.MaxJobThreadCount; i++)
+        {
+            randomArray[i] = new Unity.Mathematics.Random((uint)seed.Next());
+        }
+
+        RandomArray = new NativeArray<Unity.Mathematics.Random>(randomArray, Allocator.Persistent);
+    }
+
+    protected override void OnDestroy()
+    {
+        RandomArray.Dispose();
+    }
+
+    protected override void OnUpdate()
+    {
+    }
+}
